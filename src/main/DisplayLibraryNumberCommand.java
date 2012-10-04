f18fab6d65767e9c8d1a09cbae7d53a7aee59992
Lakshmi R P <rp.lakshmi.45@gmail.com>
2012-10-04 17:34:13 +0530

    Fourth Commit

--- src/main/DisplayLibraryNumberCommand.java
+++ src/main/DisplayLibraryNumberCommand.java
@@ -1,16 +1,16 @@
 package main;
 
-
-public class DisplayLibraryNumberCommand implements Command{
+public class DisplayLibraryNumberCommand implements Command
+{
     Boolean loggedIn;
     String savedLibraryNumber;
+
     DisplayLibraryNumberCommand(Boolean loggedIn,String savedLibraryNumber){
         this.loggedIn = loggedIn;
         this.savedLibraryNumber = savedLibraryNumber;
     }
     @Override
     public void execute() {
-
         if (loggedIn) {
             System.out.println("\n");
             System.out.println("Your library number is " + savedLibraryNumber);
--- src/main/Program.java
+++ src/main/Program.java
@@ -10,7 +10,6 @@ public class Program {
 
 
     public static void main(String[] args) {
-
         while (true) {
             printWelcome();
             printMenu();
@@ -24,21 +23,13 @@ public class Program {
             }
             else if (menuOption == 2) {
                 new ReserveBookCommand().execute();
+
             }
             else if (menuOption == 3) {
                 new DisplayLibraryNumberCommand(loggedIn,savedLibraryNumber).execute();
             }
             else if (menuOption == 4) {
-                System.out.println(createMovie("Rocky", "John G. Avildsen", "10"));
-                System.out.println(createMovie("Rocky II", "John G. Avildsen", "9"));
-                System.out.println(createMovie("Rocky III", "John G. Avildsen", "8"));
-                System.out.println(createMovie("Rocky IV", "John G. Avildsen", "7"));
-                System.out.println(createMovie("Rocky V", "John G. Avildsen", "8"));
-                System.out.println(createMovie("Drainage", "Francisco Trindade", "N/A"));
-                System.out.println(createMovie("The Shawshank Redemption", "Frank Darabont", "10"));
-                System.out.println(createMovie("The Godfather", "Francis Ford Coppola", "7"));
-                System.out.println(createMovie("Inception", "Frank Darabont", "10"));
-                System.out.println(createMovie("Pulp Fiction", "Quentin Tarantino", "6"));
+                new PrintMovieListCommand().execute();
             }
             else if (menuOption == 5) {
                 clearLogin();
@@ -118,8 +109,6 @@ public class Program {
         savedLibraryNumber = "";
     }
 
-    private static String createMovie(String movieTitle, String movieDirector, String movieRanking) {
-        return movieTitle + " - Director: " + movieDirector + " Rating: " + movieRanking;
-    }
+
 }
 
